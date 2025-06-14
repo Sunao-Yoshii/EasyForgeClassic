a501dc1b1cf76869139fea72ca74a52b3931b486
Sunao-Yoshii <2810066+Sunao-Yoshii@users.noreply.github.com>
2025-06-14 22:35:12 +0900

    デフォルトのインストールモデルを指定

--- Download/illustrious_minimum.bat
+++ Download/illustrious_minimum.bat
@@ -3,6 +3,4 @@ chcp 65001 > NUL
 @REM Minimum は Civitai ログイン不要を維持する
 
 call %~dp0src\illustriousCommon_Minimum.bat
-
-call %~dp0Stable-diffusion\NoobE\copycatNoob_v11.bat
-call %~dp0Stable-diffusion\NoobE\HarmoniqMixSpoE_v11.bat
+call %~dp0Stable-diffusion\illustrious\copycat-illustrious_v7.0.bat
