273c955ecc758d71082fa73ef1fe4417e376a9ae
Sunao-Yoshii <2810066+Sunao-Yoshii@users.noreply.github.com>
2025-06-14 18:59:34 +0900

    Testing install

---
+++ EasyForgeClassic/ForgeClassic/ForgeClassic.bat
@@ -0,0 +1,46 @@
+@echo off
+chcp 65001 > NUL
+set CURL_CMD=C:\Windows\System32\curl.exe -kL
+set EASY_TOOLS=%~dp0..\..\EasyTools
+set GITHUB_CLONE_OR_PULL=%EASY_TOOLS%\Git\GitHub_CloneOrPull.bat
+
+pushd %~dp0..\..
+
+@REM echo https://github.com/Haoming02/sd-webui-forge-classic
+call %GITHUB_CLONE_OR_PULL% Haoming02 sd-webui-forge-classic main e03fda0b0af3bb2f43aaa7b18bc06e7338dd6338
+if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
+
+popd
+pushd %~dp0..\..\sd-webui-forge-classic
+
+call %EASY_TOOLS%\Python\Python_Activate.bat
+if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
+
+echo python -m pip install -qq -U pip setuptools wheel
+python -m pip install -qq -U pip setuptools wheel
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+echo pip install -qq triton-windows==3.3.1.post19
+pip install -qq triton-windows==3.3.1.post19
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+set "TRITON_CACHE=C:\Users\%USERNAME%\.triton\cache"
+set "TORCH_INDUCTOR_TEMP=C:\Users\%USERNAME%\AppData\Local\Temp\torchinductor_%USERNAME%"
+
+if not exist "%TRITON_CACHE%" ( goto :EASY_TRITON_CACHE_NOT_FOUND )
+echo rmdir /S /Q "%TRITON_CACHE%"
+rmdir /S /Q "%TRITON_CACHE%"
+@REM if %ERRORLEVEL% neq 0 ( pause & exit /b 1 )
+:EASY_TRITON_CACHE_NOT_FOUND
+
+if not exist "%TORCH_INDUCTOR_TEMP%" ( goto :EASY_TORCH_INDUCTOR_TEMP_NOT_FOUND )
+echo rmdir /S /Q "%TORCH_INDUCTOR_TEMP%"
+rmdir /S /Q "%TORCH_INDUCTOR_TEMP%"
+@REM if %ERRORLEVEL% neq 0 ( pause & exit /b 1 )
+:EASY_TORCH_INDUCTOR_TEMP_NOT_FOUND
+
+echo pip install -qq https://github.com/woct0rdho/SageAttention/releases/download/v2.1.1-windows/sageattention-2.1.1+cu128torch2.7.1-cp310-cp310-win_amd64.whl
+pip install -qq https://github.com/woct0rdho/SageAttention/releases/download/v2.1.1-windows/sageattention-2.1.1+cu128torch2.7.1-cp310-cp310-win_amd64.whl
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+popd
---
+++ EasyForgeClassic/Setup.bat
@@ -0,0 +1,31 @@
+@echo off
+chcp 65001 > NUL
+
+call %~dp0ForgeClassic\ForgeClassic.bat
+if %ERRORLEVEL% neq 0 ( exit /b 1 )
+
+call %~dp0ForgeClassic\ReforgeExtension.bat
+if %ERRORLEVEL% neq 0 ( exit /b 1 )
+
+call %~dp0ForgeClassic\ReforgeLink.bat
+if %ERRORLEVEL% neq 0 ( exit /b 1 )
+
+if exist %~dp0vc_redist.x64.exe ( goto :EXIST_VC_REDIST_X64 )
+echo.
+echo %CURL_CMD% -o %~dp0vc_redist.x64.exe https://aka.ms/vs/17/release/vc_redist.x64.exe
+%CURL_CMD% -o %~dp0vc_redist.x64.exe https://aka.ms/vs/17/release/vc_redist.x64.exe
+if %ERRORLEVEL% neq 0 ( pause & exit /b 1 )
+:EXIST_VC_REDIST_X64
+
+if not exist %~dp0..\stable-diffusion-webui-forge\ ( goto :SKIP_FORGE_UPDATE )
+call %~dp0SetupForge.bat
+if %ERRORLEVEL% neq 0 ( exit /b 1 )
+:SKIP_FORGE_UPDATE
+
+if exist %~dp0Reforge\Update_DisableMinimumDownload.txt ( exit /b 0 )
+
+if exist %~dp0..\Model\Stable-diffusion\NoobE\ (
+	@REM call %~dp0..\Download\NoobAiEpsilonPred_Minimum.bat
+	call %~dp0..\Download\src\NoobAiCommon_Minimum.bat
+	@REM リンク切れ対策としてダウンロードの結果は確認しない
+)
